329fc38321338e630fe5c283958ffe6aded6e82a
occiandiaali <ocean.diaali@gmail.com>
2020-07-17 16:56:37 +0100

    Initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,139 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { Platform, ToastController } from '@ionic/angular';
+import { Base64ToGallery, Base64ToGalleryOptions } from '@ionic-native/base64-to-gallery/ngx';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
+  @ViewChild('imageCanvas', { static: false }) canvas: any;
+  canvasElement: any;
+  saveX: number;
+  saveY: number;
 
-  constructor() {}
+  selectedColor = '#9e2956';
+  colors = ['#9e2956', '#c2281d', '#de722f', '#edbf4c', '#5db37e', '#459cde', '#4250ad', '#802fa3' ];
+  drawing = false;
+  lineWidth = 5;
 
-}
+  constructor(private plt: Platform, private base64ToGallery: Base64ToGallery, private toastCtrl: ToastController) {}
+
+  ngAfterViewInit() {
+    // Set the Canvas Element and its size
+    this.canvasElement = this.canvas.nativeElement;
+    this.canvasElement.width = this.plt.width() + '';
+    this.canvasElement.height = 200;
+  }
+
+  startDrawing(ev) {
+    this.drawing = true;
+    const canvasPosition = this.canvasElement.getBoundingClientRect();
+
+    this.saveX = ev.pageX - canvasPosition.x;
+    this.saveY = ev.pageY - canvasPosition.y;
+  }
+
+  endDrawing() {
+    this.drawing = false;
+  }
+
+  selectColor(color) {
+    this.selectedColor = color;
+  }
+
+  setBackground() {
+    const background = new Image();
+    background.src = '../../assets/icon/favicon.png';
+    const ctx = this.canvasElement.getContext('2d');
+
+    background.onload = () => {
+      ctx.drawImage(background, 0, 0, this.canvasElement.width, this.canvasElement.height);
+    };
+  } // set background
+
+  moved(ev) {
+    if (!this.drawing) { return; }
+
+    const canvasPosition = this.canvasElement.getBoundingClientRect();
+    const ctx = this.canvasElement.getContext('2d');
+
+    const currentX = ev.pageX - canvasPosition.x;
+    const currentY = ev.pageY - canvasPosition.y;
+
+    ctx.lineJoin = 'round';
+    ctx.strokeStyle = this.selectedColor;
+    ctx.lineWidth = this.lineWidth;
+
+    ctx.beginPath();
+    ctx.moveTo(this.saveX, this.saveY);
+    ctx.lineTo(currentX, currentY);
+    ctx.closePath();
+
+    ctx.stroke();
+
+    this.saveX = currentX;
+    this.saveY = currentY;
+  }
+
+  exportCanvasImage() {
+    const dataUrl = this.canvasElement.toDataURL();
+
+    // Clear the current canvas
+    const ctx = this.canvasElement.getContext('2d');
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    if (this.plt.is('cordova')) {
+      const options: Base64ToGalleryOptions = { prefix: 'canvas_', mediaScanner:  true };
+
+      this.base64ToGallery.base64ToGallery(dataUrl, options).then(
+        async res => {
+          const toast = await this.toastCtrl.create({
+            message: 'Image saved to camera roll.',
+            duration: 2000
+          });
+          toast.present();
+        },
+        err => console.log('Error saving image to gallery ', err)
+      );
+    } else {
+      // Fallback for Desktop
+      const data = dataUrl.split(',')[1];
+      const blob = this.b64toBlob(data, 'image/png');
+
+      const a = window.document.createElement('a');
+      a.href = window.URL.createObjectURL(blob);
+      a.download = 'canvasimage.png';
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    }
+  }
+
+  // https://forum.ionicframework.com/t/save-base64-encoded-image-to-specific-filepath/96180/3
+  b64toBlob(b64Data, contentType) {
+    contentType = contentType || '';
+    const sliceSize = 512;
+    const byteCharacters = atob(b64Data);
+    const byteArrays = [];
+
+    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+      const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+      const byteNumbers = new Array(slice.length);
+      for (let i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
+      }
+
+      const byteArray = new Uint8Array(byteNumbers);
+
+      byteArrays.push(byteArray);
+    }
+
+    const blob = new Blob(byteArrays, { type: contentType });
+    return blob;
+  }
+
+} // class
